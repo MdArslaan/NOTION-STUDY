c25a5b2cb8a460c680569d8ea19de1be2255a498
MdArslaan <142446633+MdArslaan@users.noreply.github.com>
2025-02-03 11:59:37 +0530

    Configured Server

--- server/index.js
+++ server/index.js
@@ -23,7 +23,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		// origin:"http://localhost:3000",
+		origin:"https://notion-study.onrender.com",
 		credentials:true,
 	})
 )
